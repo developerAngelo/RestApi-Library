fdef5d7c57933b3ba24c68543991055ff686f2ee
Angelo <devsangelo@gmail.com>
2021-05-05 16:44:26 +0800

    Building RestFul API Library for fetching data.

---
+++ RestApi-Library/Stuctures/UserDataStucture.swift
@@ -0,0 +1,56 @@
+//
+//  UserDataStucture.swift
+//  RestApi-Library
+//
+//  Created by Ruthlyn Huet on 5/5/21.
+//
+
+import Foundation
+
+
+var defString = String(stringLiteral: "")
+var defInt = -1
+
+struct UserData: Codable, CustomStringConvertible {
+    var page: Int?
+    var perPage: Int?
+    var total: Int?
+    var totalPages: Int?
+    var data: [User]?
+    
+    var description: String {
+        var desc = """
+        page = \(page ?? defInt)
+        records per page = \(perPage ?? defInt)
+        total records = \(total ?? defInt)
+        total pages = \(totalPages ?? defInt)
+        Users:
+        
+        """
+        if let users = data {
+            for user in users {
+                desc += user.description
+            }
+        }
+        
+        return desc
+    }
+}   
+
+struct User: Codable, CustomStringConvertible {
+    var id: Int?
+    var firstName: String?
+    var lastName: String?
+    var avatar: String?
+
+    var description: String {
+        return """
+        ------------
+        id = \(id ?? defInt)
+        firstName = \(firstName ?? defString)
+        lastName = \(lastName ?? defString)
+        avatar = \(avatar ?? defString)
+        ------------
+        """
+    }
+}
--- RestApi-Library/ViewController.swift
+++ RestApi-Library/ViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let rest = RestManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Starting fetching...")
+        getUsersList()
+        
+    }
+    
+    func getUsersList() {
+
+        guard let url = URL(string: "https://reqres.in/api/users") else { return }
+
+        rest.makeRequest(toURL: url, with: .get) { (results) in
+            if let data = results.data {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                guard let userData = try? decoder.decode(UserData.self, from: data) else { return }
+                print(userData.description)
+            }
+        }
     }
 
 
